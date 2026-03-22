3645afbccb1fad83b17f4d9ffb5714236cecf132
micolo1815-ship-it <micolo1815@gmail.com>
2026-03-23 03:01:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import './bootstrap'; // Standard Laravel bootstrap file
+import 'bootstrap';    // The actual Bootstrap JS library
 
 export default defineConfig({
     plugins: [
