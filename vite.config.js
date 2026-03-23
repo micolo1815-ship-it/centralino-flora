eeb75e050a27c05aca4e672e6119476cf46285e1
micolo1815-ship-it <micolo1815@gmail.com>
2026-03-24 04:23:31 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,21 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            // Vite processes this file...
             input: ['resources/css/style.css'],
-            // ...and puts it in public/build/assets/style.css
+            refresh: true,
         }),
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                // This forces the file name to be exactly "style.css" 
+                // so your asset() link doesn't break
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name === 'style.css') return 'style.css';
+                    return 'assets/[name]-[hash][extname]';
+                },
+            },
+        },
+    },
 });
